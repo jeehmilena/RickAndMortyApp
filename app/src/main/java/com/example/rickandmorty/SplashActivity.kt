b9f4c95de038274c0cbe935ef8addfd317428709
jeehmilena <jessicamilenacorrea@gmail.com>
2020-04-01 19:19:23 -0300

    implementeçao inicial

--- app/src/main/java/com/example/rickandmorty/SplashActivity.kt
+++ app/src/main/java/com/example/rickandmorty/SplashActivity.kt
@@ -1,12 +1,30 @@
 package com.example.rickandmorty
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import android.content.Intent
+import com.example.rickandmorty.home.view.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    val timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                jump()
+            }
+        }, 3000)
+    }
+
+    private fun jump() {
+        timer.cancel()
+        startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+        finish()
+
     }
 }
